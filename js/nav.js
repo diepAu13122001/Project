0a93e48830fe4a7b91bfd7979d801975e0544a8e
diepAu13122001 <91660142+diepAu13122001@users.noreply.github.com>
2021-12-28 00:48:08 +0700

    change link for nav page

--- js/nav.js
+++ js/nav.js
@@ -3,7 +3,7 @@ const createNav = () => {
 
     nav.innerHTML = `
              <div class="nav">
-             <a href="../public/home.html"> <img src="img/dark-logo.png" class="brand-logo" alt=""> </a>
+             <a href="../index.html"> <img src="img/dark-logo.png" class="brand-logo" alt=""> </a>
                 <div class="nav-items">
                     <div class="search">
                         <input type="text" class="search-box" placeholder="search brand, product">
